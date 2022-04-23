180fae24d1a0ea444544383395ae440b37a0c8a2
VanessaYoshida <vanessayoshida92@gmail.com>
2022-04-23 18:21:05 -0300

    Novo teste na controller

--- src/test/java/com/bootcamp/microservicemeetup/controller/MeetupControllerTest.java
+++ src/test/java/com/bootcamp/microservicemeetup/controller/MeetupControllerTest.java
@@ -5,8 +5,12 @@ import com.bootcamp.microservicemeetup.exception.BusinessException;
 import com.bootcamp.microservicemeetup.controller.dto.MeetupDTO;
 import com.bootcamp.microservicemeetup.model.entity.Meetup;
 import com.bootcamp.microservicemeetup.model.entity.Registration;
+import com.bootcamp.microservicemeetup.repository.MeetupRepository;
 import com.bootcamp.microservicemeetup.service.MeetupService;
 import com.bootcamp.microservicemeetup.service.RegistrationService;
+import com.bootcamp.microservicemeetup.service.impl.MeetupServiceImpl;
+import com.bootcamp.microservicemeetup.service.impl.RegistrationServiceImpl;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -24,6 +28,7 @@ import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilde
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.testcontainers.shaded.com.fasterxml.jackson.databind.ObjectMapper;
 
+import static org.mockito.ArgumentMatchers.anyInt;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 import java.util.Optional;
@@ -32,6 +37,7 @@ import java.util.Optional;
 @ActiveProfiles("test")
 @WebMvcTest(controllers = {MeetupController.class})
 @AutoConfigureMockMvc
+
 public class MeetupControllerTest {
 
     static final String MEETUP_API = "/api/meetups";
@@ -40,10 +46,18 @@ public class MeetupControllerTest {
     MockMvc mockMvc;
 
     @MockBean
-    private RegistrationService registrationService;
+    RegistrationService registrationService;
 
     @MockBean
-    private MeetupService meetupService;
+    MeetupService meetupService;
+
+    @MockBean
+    MeetupRepository meetupRepository;
+
+//    @BeforeEach
+//    public void setUp() {
+//        this.meetupService = new MeetupServiceImpl(meetupRepository);
+//    }
 
     @Test
     @DisplayName("Should register on a meetup")
@@ -125,13 +139,15 @@ public class MeetupControllerTest {
 
         Integer idRegistration = 44;
 
-        MeetupDTO dto = MeetupDTO.builder().registrationAttribute("234").event("Womakerscode Dados").build();
+        MeetupDTO.builder().registrationAttribute("234").event("Womakerscode Dados").build();
 
         Registration registration = Registration.builder().id(idRegistration).registration("234").build();
 
-        Meetup meetup = Meetup.builder().id(idRegistration).event("Womakerscode Dados").registration(registration).meetupDate("10/10/2022").build();
+        Meetup meetup = Meetup.builder().id(idRegistration).event("Womakerscode Dados").registration(registration).meetupDate("10/10/2022").registered(true).build();
 
         String json = new ObjectMapper().writeValueAsString(meetup);
+
+        BDDMockito.given(meetupService.save(Mockito.any(Meetup.class))).willReturn(meetup);
 
         BDDMockito.given(meetupService.getById(idRegistration)).willReturn(Optional.of(meetup));
 
@@ -143,9 +159,61 @@ public class MeetupControllerTest {
 
         mockMvc.perform(request)
                 .andExpect(status().isOk());
-//                .andExpect(jsonPath("id").value(44))
+//                .andExpect(jsonPath("id").value(idRegistration))
 //                .andExpect(jsonPath("event").value("Womakerscode Dados"))
 //                .andExpect(jsonPath("registration").value(registration))
 //                .andExpect(jsonPath("meetupDate").value("10/10/2022"));
     }
+
+    @Test
+    @DisplayName("Should return empty when get an meetup registration by id when doesn't exists")
+    public void meetupRegistrationNotFound() throws Exception {
+
+        Integer idRegistration = 44;
+
+        MeetupDTO.builder().registrationAttribute("234").event("Womakerscode Dados").build();
+
+        Registration.builder().id(idRegistration).registration("234").build();
+
+        BDDMockito.given(meetupService.getById(idRegistration)).willReturn(Optional.empty());
+
+        MockHttpServletRequestBuilder request = MockMvcRequestBuilders
+                .get(MEETUP_API.concat("/" + idRegistration))
+                .accept(MediaType.APPLICATION_JSON);
+
+        mockMvc.perform(request)
+                .andExpect(status().isNotFound());
+    }
+
+    @Test
+    @DisplayName("Should delete the meetup registration")
+    public void deleteMeetupRegistration() throws Exception {
+
+        Integer idRegistration = 44;
+
+        MeetupDTO.builder().registrationAttribute("234").event("Womakerscode Dados").build();
+
+        Registration.builder().id(idRegistration).registration("234").build();
+
+        BDDMockito.given(meetupService.getById(idRegistration)).willReturn(Optional.empty());
+
+        MockHttpServletRequestBuilder request = MockMvcRequestBuilders
+                .get(MEETUP_API.concat("/" + idRegistration))
+                .accept(MediaType.APPLICATION_JSON);
+
+        mockMvc.perform(request)
+                .andExpect(status().isNotFound());
+
+        BDDMockito.given(meetupService.getById(anyInt()))
+                .willReturn(Optional.of(Registration.builder().id(11).build()));
+
+        MockHttpServletRequestBuilder requestBuilder = MockMvcRequestBuilders
+                .delete(MEETUP_API.concat("/" + idRegistration))
+                .accept(MediaType.APPLICATION_JSON);
+
+        mockMvc.perform(requestBuilder)
+                .andExpect(status().isNoContent());
+    }
+
+
 }
